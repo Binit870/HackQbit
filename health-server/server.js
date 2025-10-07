10e5057ee5ff6f4b4e9d6e6d89eff98b43f6df8a
arjunsingh9602 <7061309625arjun@gmail.com>
2025-10-08 00:14:08 +0530

    initial

--- health-server/server.js
+++ health-server/server.js
@@ -18,7 +18,7 @@ const app = express();
 const server = createServer(app);
 
 // ✅ Allowed origins
-const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"];
+const allowedOrigins = ["http://localhost:5173","http://localhost:5174", "http://127.0.0.1:5173"];
 
 // ✅ CORS setup
 app.use(
